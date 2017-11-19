867ee7071cd5ea7b0ca9279772e1e1eb519db595
Weiyuan <ax61316@hotmail.com>
2017-11-19 14:04:16 +0800

    Add check mat type snippet

--- opencv_using_opencl_kernel_example/main.cpp
+++ opencv_using_opencl_kernel_example/main.cpp
@@ -7,6 +7,29 @@
 
 using namespace std;
 
+string type2str(int type) {
+  string r;
+
+  uchar depth = type & CV_MAT_DEPTH_MASK;
+  uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch ( depth ) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+  }
+
+  r += "C";
+  r += (chans+'0');
+
+  return r;
+}
+
 int main()
 {
     // check OpenCL availability
@@ -40,9 +63,16 @@ int main()
     cv::ocl::Device(context.device(0));
 
     // Transfer Mat data to the device
-    cv::Mat mat_src = cv::imread("/home/alex504/img_video_file/test_img.jpg", cv::IMREAD_GRAYSCALE);
+    cv::Mat mat_src = cv::imread("/home/alex504/img_video_file/test_img.jpeg", cv::IMREAD_GRAYSCALE);
     cv::UMat umat_src = mat_src.getUMat(cv::ACCESS_READ, cv::USAGE_ALLOCATE_DEVICE_MEMORY);
     cv::UMat umat_dst(mat_src.size(), mat_src.type(), cv::ACCESS_WRITE, cv::USAGE_ALLOCATE_DEVICE_MEMORY);
+    
+    string ty =  type2str( mat_src.type() );
+    printf("Matrix: %s %dx%d \n", ty.c_str(), mat_src.cols, mat_src.rows );
+    ty =  type2str( umat_src.type() );
+    printf("Matrix: %s %dx%d \n", ty.c_str(), umat_src.cols, umat_src.rows );
+    ty =  type2str( umat_dst.type() );
+    printf("Matrix: %s %dx%d \n", ty.c_str(), umat_dst.cols, umat_dst.rows );
 
     // Read the OpenCL kernel code
     std::ifstream ifs("neg.cl");
@@ -61,9 +91,9 @@ int main()
     // kernel argument
     kernel.args(cv::ocl::KernelArg::ReadOnlyNoSize(umat_src), cv::ocl::KernelArg::ReadWrite(umat_dst));
 
-    size_t globalThreads[3] = { mat_src.cols, mat_src.rows, 1 };
+    size_t globalThreads[3] = { (size_t)mat_src.cols, (size_t)mat_src.rows, 1 };
     size_t localThreads[3] = { 16, 16, 1 };
-    
+
     bool success = kernel.run(3, globalThreads, NULL, true);
     if (!success){
         cout << "Failed running the kernel..." << endl;
