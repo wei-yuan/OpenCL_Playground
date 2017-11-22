d55cc29b2f0f21e9603c57250b3602a1929884b3
Weiyuan <ax61316@hotmail.com>
2017-11-22 10:14:12 +0800

    Work on calculation of errorCovPre matrix

--- kalman_filter/main.cpp
+++ kalman_filter/main.cpp
@@ -8,6 +8,9 @@
 
 int main()
 {
+    // ---------------------------------------------------
+    // OpenCL Initialization
+    // ---------------------------------------------------
     // check OpenCL availability
     if (!cv::ocl::haveOpenCL())
     {
@@ -37,24 +40,26 @@ int main()
     // Select the first device
     cv::ocl::Device(context.device(0));
 
+    // ---------------------------------------------------
+    // Data Initialization
+    // ---------------------------------------------------
     // Kalman filter here
     cv::KalmanFilter KF(16, 8, 0);
     // KF.transitionMatrix needs to be CV_32F or CV_64F
-    std::cout << "KF.transitionMatrix(A): \n" << KF.transitionMatrix << std::endl;
-
-    int Mat_type = CV_32F;
-    cv::Mat src1 = cv::Mat::ones(4, 4, CV_8UC1);
-    src1.convertTo(src1, Mat_type, 1);
-    cv::Mat src2 = cv::Mat::ones(4, 4, CV_8UC1);
-    src1.convertTo(src2, Mat_type, 1);
-    std::cout << "src1: \n" << src1 << std::endl;    
-    std::cout << "src2: \n" << src2 << std::endl;
+    std::cout << "KF.transitionMatrix(A): \n" << KF.transitionMatrix << std::endl;    
+    cv::Mat inputMeas = cv::Mat::ones(16, 1, CV_32F);
+    cv::Mat mat_dst = cv::Mat::ones(16, 1, CV_32F);
+    std::cout << "KF.transitionMatrix: \n" << KF.transitionMatrix << std::endl;    
+    std::cout << "inputMeas: \n" << inputMeas << std::endl;
     
     // Transfer Mat data to the device
-    cv::UMat usrc1 = src1.getUMat(cv::ACCESS_READ, cv::USAGE_ALLOCATE_DEVICE_MEMORY);
-    cv::UMat usrc2 = src2.getUMat(cv::ACCESS_READ, cv::USAGE_ALLOCATE_DEVICE_MEMORY);    
-    cv::UMat umat_dst(src1.size(), src1.type(), cv::ACCESS_WRITE, cv::USAGE_ALLOCATE_DEVICE_MEMORY);
+    cv::UMat utransitionMatrix = KF.transitionMatrix.getUMat(cv::ACCESS_READ, cv::USAGE_ALLOCATE_DEVICE_MEMORY);
+    cv::UMat uinputMeas = inputMeas.getUMat(cv::ACCESS_READ, cv::USAGE_ALLOCATE_DEVICE_MEMORY);    
+    cv::UMat umat_dst(mat_dst.size(), mat_dst.type(), cv::ACCESS_WRITE, cv::USAGE_ALLOCATE_DEVICE_MEMORY);
 
+    // ---------------------------------------------------
+    // Kernel Initialization
+    // ---------------------------------------------------
     // Read the OpenCL kernel code
     std::ifstream ifs("kalman_predict.cl");
     if (ifs.fail()) 
@@ -66,40 +71,57 @@ int main()
     cv::String errmsg;
     // By setting program argument "-D xxx=yyy ", we can replace xxx with yyy in the kernel
     cv::String buildopt =""; 
-    cv::ocl::Program program = context.getProg(programSource, buildopt, errmsg);
+    cv::ocl::Program program = context.getProg(programSource, buildopt, errmsg);    
 
-    for(int i=0; i< 2;i++)
-    {   
-        std::cout << "usrc1 step[" << i << "]:" << usrc1.step[i] << std::endl;             
-        std::cout << "umat_src step[" << i << "]:" << umat_dst.step[i] << std::endl;        
-    }
-    std::cout << "umat_src offset: " << usrc1.offset 
-              << "\numat_dst offset: " << umat_dst.offset << std::endl;
-
+    // ---------------------------------------------------
+    // Create Kernel and run
+    // ---------------------------------------------------    
     // create kernel
-    cv::ocl::Kernel kernel("kalman_predict", program);
+    cv::ocl::Kernel kernel_statePre("kalman_predict", program);
     // kernel argument
-    kernel.args(Mat_type,
-                cv::ocl::KernelArg::ReadOnlyNoSize(usrc1), 
-                cv::ocl::KernelArg::ReadOnlyNoSize(usrc2),
-                cv::ocl::KernelArg::ReadWrite(umat_dst));    
+    int Mat_type = CV_32F;    
+    kernel_statePre.args(Mat_type, 
+                cv::ocl::KernelArg::ReadOnlyNoSize(utransitionMatrix),
+                cv::ocl::KernelArg::ReadOnlyNoSize(uinputMeas),
+                cv::ocl::KernelArg::ReadWrite(umat_dst));
 
-    size_t globalThreads[3] = { (size_t)src1.cols, (size_t)src1.rows, 1 };
+    size_t globalThreads[3] = { (size_t)KF.transitionMatrix.cols, (size_t)KF.transitionMatrix.rows, 1 };
     //size_t localThreads[3] = { 16, 16, 1 };
 
-    bool success = kernel.run(3, globalThreads, NULL, true);
+    bool success = kernel_statePre.run(3, globalThreads, NULL, true);
+    if (!success){
+        std::cout << "Failed running the kernel..." << std::endl;
+        return -1;
+    }
+    // ---------------------------------------------------
+    // Copy data from device(GPU) to host(CPU)
+    // ---------------------------------------------------     
+    // Download the dst data from the device
+    mat_dst = umat_dst.getMat(cv::ACCESS_READ);
+    std::cout << "\nmat_dst: \n" << mat_dst << std::endl;
+
+
+    // create kernel
+    cv::ocl::Kernel kernel_errorCovPre("kalman_predict", program);
+    // kernel argument
+    int Mat_type = CV_32F;
+    kernel_errorCovPre.args(Mat_type, 
+                cv::ocl::KernelArg::ReadOnlyNoSize(utransitionMatrix),
+                cv::ocl::KernelArg::ReadOnlyNoSize(uinputMeas),
+                cv::ocl::KernelArg::ReadWrite(umat_dst));
+
+    size_t globalThreads[3] = { (size_t)KF.transitionMatrix.cols, (size_t)KF.transitionMatrix.rows, 1 };
+    //size_t localThreads[3] = { 16, 16, 1 };
+
+    bool success = kernel_errorCovPre.run(3, globalThreads, NULL, true);
     if (!success){
         std::cout << "Failed running the kernel..." << std::endl;
         return -1;
     }
 
     // Download the dst data from the device (?)
-    cv::Mat mat_dst = umat_dst.getMat(cv::ACCESS_READ);
+    mat_dst = umat_dst.getMat(cv::ACCESS_READ);
     std::cout << "\nmat_dst: \n" << mat_dst << std::endl;
-/*
-    cv::imshow("src", mat_src);
-    cv::imshow("dst", mat_dst);
-    cv::waitKey();
-*/
+
     return 0;
 }
